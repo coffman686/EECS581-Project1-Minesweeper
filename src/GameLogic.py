9c3d0204b8d9a220b316300314676d95ada044c1
soiledit <no.sleep410@passinbox.com>
2025-09-09 14:17:43 -0500

    initialize game logic functionality

---
+++ src/GameLogic.py
@@ -0,0 +1,171 @@
+import random
+import enum
+
+# TODO: remove random seeding
+random.seed(0)
+
+
+class Cell:
+  def __init__(self):
+    self.covered = True
+    self.flagged = False
+    self.mine = False
+    self.nearby = 0
+
+
+class GameState(enum.Enum):
+  Start = 0
+  Playing = 1
+  EndWin = 2
+  EndLose = 3
+
+
+class EndCondition(enum.Enum):
+  Win = True
+  Loss = False
+
+
+type Board = list[list[Cell]]
+
+
+# stub for board
+board = []
+for _ in range(10):
+  row = []
+  for _ in range(10):
+    row.append(Cell())
+  board.append(row)
+
+
+class GameLogic:
+  def __init__(self, board):
+    self.state: GameState = GameState.Start
+    self.total_mines: int = 0
+    self.flags_remaining: int = 0
+    self.covered_cells: int = 0
+    self.board: list[list[Cell]] = board
+
+  def set_mines(self, mines):
+    self.total_mines = mines
+
+  def start_game(self):
+    self.state = GameState.Playing
+    self.initialize_board()
+
+  def end_game(self, win: EndCondition):
+    if win:
+      self.state = GameState.EndWin
+    else:
+      self.state = GameState.EndLose
+
+  def convert_coord_to_indices(self, id) -> tuple[int, int]:
+    return (id // 10, id % 10)
+
+  def initialize_board(self):
+    mines_coordinates = random.sample(range(100), k=self.total_mines)
+    for coord in mines_coordinates:
+      row, col = self.convert_coord_to_indices(coord)
+      self.place_mine(row, col)
+
+  def valid_position(self, row, col):
+    return 0 <= row <= 9 and 0 <= col <= 9
+
+  def place_mine(self, row, col):
+    self.board[row][col].mine = True
+    for i in range(row - 1, row + 2):
+      for j in range(col - 1, col + 2):
+        if self.valid_position(i, j):
+          self.board[i][j].nearby += 1
+
+  def remove_mine(self, row, col):
+    self.board[row][col].mine = False
+    for i in range(row - 1, row + 2):
+      for j in range(col - 1, col + 2):
+        if self.valid_position(i, j):
+          self.board[i][j].nearby -= 1
+
+  def uncover_first_cell(self, old_row, old_col):
+    while True:
+      new_row, new_col = self.convert_coord_to_indices(random.randrange(100))
+      new_cell = self.board[new_row][new_col]
+      if not new_cell.mine:
+        self.place_mine(new_row, new_col)
+        break
+    self.remove_mine(old_row, old_col)
+
+  def uncover_cell(self, row, col, first_cell: bool = False):
+    cell = self.board[row][col]
+
+    if first_cell and cell.mine:
+      self.uncover_first_cell(row, col)
+
+    if cell.mine:
+      self.end_game(EndCondition.Loss)
+    else:
+      cell.covered = False
+      self.covered_cells -= 1
+
+      if cell.nearby:
+        return
+
+      for i in range(row - 1, row + 2):
+        for j in range(col - 1, col + 2):
+          if not self.valid_position(i, j):
+            continue
+          cell = self.board[i][j]
+          if not cell.mine and cell.covered:
+            self.uncover_cell(i, j)
+
+    if self.covered_cells == 0:
+      self.end_game(EndCondition.Win)
+
+  def toggle_flagged_cell(self, row, col):
+    cell = self.board[row][col]
+    if cell.flagged:
+      if self.flags_remaining > 0:
+        cell.flagged = False
+        self.flags_remaining -= 1
+    else:
+      if self.flags_remaining < 10:
+        cell.flagged = True
+        self.flags_remaining += 1
+
+  def print_board(self, debug: bool = False):
+    for row in self.board:
+      for cell in row:
+        if cell.flagged:
+          print(" ", end="")
+        if not debug and cell.covered:
+          print(" ", end="")
+          continue
+        if cell.mine:
+          print(" ", end="")
+        else:
+          print(f"{cell.nearby} ", end="")
+      print("")
+
+  def __repr__(self):
+    print(self.state)
+    self.print_board(debug=True)
+    return ""
+
+
+logic = GameLogic(board)
+logic.set_mines(10)
+logic.start_game()
+
+logic.print_board(True)
+
+first = True
+while True:
+  logic.print_board()
+  inp = int(input("Enter coordinate:"))
+  row, col = logic.convert_coord_to_indices(inp)
+
+  logic.uncover_cell(row, col, first)
+  if first:
+    first = False
+
+  if logic.state == GameState.EndWin or logic.state == GameState.EndLose:
+    print("Win!")
+    break
