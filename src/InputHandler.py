ebe0ad4438801b79fbe55cefa606de9a85bb5781
samsuggs <samsuggs@ku.edu>
2025-09-17 12:17:21 -0500

    Add prologue and inline comments

--- src/InputHandler.py
+++ src/InputHandler.py
@@ -1,15 +1,28 @@
+"""
+PROLOGUE
+Module: InputHandler
+Description: Contains InputHandler class with functions that handle user clicks and keyboard
+             inputs and returns updated game state and the type of response that was given
+Inputs: None
+Outputs: None
+External Sources: n/a
+Created At: 9/10/2025 by Sam Suggs
+"""
+
+# Import required libraries and classes
 import pygame
 import enum
-from GameLogic import GameLogic, GameState, Cell
+from GameLogic import GameLogic, GameState
 
-# enum to handle the type of response given by the input handler
+# enum class to handle the type of response given by the input handler
 class ResponseCode(enum.Enum):
     Finished = 0
     Failed = 1
     InProgress = 2
     Ignored = 3
 
-# response object designed to package and send input handling results
+# Response object designed to package and send input handling results
+# Inputs: game: GameLogic, response_code: ResponseCode, message: str
 class Response:
     def __init__(self, game, response_code, message=''):
         self.game: GameLogic = game
@@ -20,143 +33,72 @@ class Response:
 class InputHandler:
     # Keyboard input handling
     # Will be used at the beginning of the game when user enters number of mines
+    # Inputs: game: GameLogic, event: PygameEvent, text: str
+    # Output: Response
     def handle_keyboard_input(self, game, event, text):
+        # Keyboard inputs will only be accepted at the start of the game
+        # (that's when a user will enter the number of mines they want)
         if game.state != GameState.Start:
             return Response(game, ResponseCode.Ignored, "Game must be in starting state")
         #if keyboard input
         if event.type == pygame.KEYDOWN:
             #if enter (user is done typing)
             if event.key == pygame.K_RETURN:
+                # verify that the input is a number
                 if text.isdigit():
+                    # convert to int
                     mines = int(text)
+                    # if it's a legal amount of mines, update the game object
                     if 10 <= mines <= 20:
                         game.set_mines(mines)
                         return Response(game, ResponseCode.Finished, f"Set {mines} mines")
+                    # otherwise return error response
                     else:
                         return Response(game, ResponseCode.Failed, "Mine count must be between 10 and 20")
+                # otherwise return error response
                 else:
                     return Response(game, ResponseCode.Failed, "Input must be a number between 10 and 20")
             #if backspace
             elif event.key == pygame.K_BACKSPACE:
+                # remove the last character from the text string
                 text = text[:-1]
+                # return response object including updated text
                 return Response(game, ResponseCode.InProgress, text)
             else:
+                # add input to text variable and return it in a response
                 text += event.unicode
                 return Response(game, ResponseCode.InProgress, text)
+        # return ignored response if input is not a keyboard input
         return Response(game, ResponseCode.Ignored, "Ignored irrelevant input")
 
     # Function to handle mouse clicks
     # This will be used to handle cell clearing, cell flagging, and resetting
+    # Inputs: game: GameLogic, event: PygameEvent, x: int, y: int
+    # Output: Response
     def handle_click(self, game, event, x, y):
+        # Click inputs will only be handled when the game is in progress
         if game.state != GameState.Playing:
             return Response(game, ResponseCode.Ignored, "Game must be in progress")
+        # if the event was a mouse click
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # ensure that the coordinates are in the proper range
             if game.valid_position:
                 # if left click -> uncover mine
                 if event.button == 1:
                     game.uncover_cell(row=y, col=x)
+                    # return updated game
                     return Response(game, ResponseCode.Finished, f"Uncovered cell at ({x}, {y})")
                 # if right click -> toggle flag
                 elif event.button == 3:
                     game.toggle_flagged_cell(row=y, col=x)
+                    # return updated game
                     return Response(game, ResponseCode.Finished, f"Toggled flag at ({x}, {y})")
+                # if not a valid mouse click, return ignored response
                 else:
                     return Response(game, ResponseCode.Ignored, "Ignored irrelevant input")
+            # if click not in range, return ignored response
             else:
                 return Response(game, ResponseCode.Ignored, "Click out of range")
+        # if not a click, return ignored response
         else:
             return Response(game, ResponseCode.Ignored, "Ignored irrelevant input")
-
-
-# The following code is temporary and is used for testing
-
-# stub for BoardManager
-type Board = list[list[Cell]]
-
-board = []
-for _ in range(10):
-    row = []
-    for _ in range(10):
-        row.append(Cell())
-    board.append(row)
-
-game = GameLogic(board)
-
-#Simple pygame instance to test the clicking handling
-def test_click_input():
-    pygame.init()
-    SCREEN_LENGTH=500
-    SCREEN_WIDTH=500
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
-
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    GRAY = (200, 200, 200)
-    GRID_SIZE = 50
-    GRID_COLOR = GRAY
-    running = True
-    input_handler = InputHandler()
-    game.state=GameState.Playing
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            else:
-                response = input_handler.handle_click(game, event)
-                print(response.message)
-
-        screen.fill(BLACK)
-
-        for x in range(0, SCREEN_WIDTH, GRID_SIZE):
-            pygame.draw.line(screen, GRID_COLOR, (x, 0), (x, SCREEN_LENGTH))
-        for y in range(0, SCREEN_LENGTH, GRID_SIZE):
-            pygame.draw.line(screen, GRID_COLOR, (0, y), (SCREEN_WIDTH, y))
-
-        pygame.display.flip()
-
-    pygame.quit()
-
-#Simple pygame instance to check the keyboard input handling
-def test_keyboard_input():
-    pygame.init()
-    SCREEN_LENGTH = 500
-    SCREEN_WIDTH = 500
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
-    font = pygame.font.Font(None, 48)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    input_handler = InputHandler()
-    text = ""
-    message=""
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            else:
-                response = input_handler.handle_keyboard_input(game, event, text)
-                print(response.message)
-                if response.response_code == ResponseCode.Finished:
-                    text = ""
-                    message = response.message
-                elif response.response_code == ResponseCode.InProgress:
-                    text = response.message
-                    message = ""
-                elif response.response_code == ResponseCode.Failed:
-                    message = response.message
-        screen.fill(WHITE)
-        text_surface = font.render(text, True, BLACK)
-        error_surface = font.render(message, True, BLACK)
-        screen.blit(text_surface, (50, 200))
-        screen.blit(error_surface, (50, 300))
-
-        pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
-
-
-#test_click_input()
-test_keyboard_input()
-        
-        
